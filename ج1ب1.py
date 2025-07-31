4c18a1af03507f92d49c32127021a0f2e6368101
Mohammed deeb Alaslan <mohammedalaslan10@gmail.com>
2025-08-01 02:33:26 +0300

    Add files via upload

---
+++ ج1ب1.py
@@ -0,0 +1,17 @@
+value =int (input ("enter value mark:"))
+if 100 >value >0 :
+    print ("eccepted value")
+    if value <60 :
+       print ("the student is field")
+    elif value <70 :
+       print ("good")
+    elif value <80 :
+       print ("very good")    
+    elif value<90 :
+       print ("exelant")
+    else :
+       print ("very exelant")    
+    
+else :
+    print ("not eccept value ")
+        
